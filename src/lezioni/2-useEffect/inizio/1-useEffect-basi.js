57fd2bb18969b2bb4dc35a48e4c628fb5eb7b962
maicolkutchoukov <maicol.j.95@gmail.com>
2024-07-07 01:27:47 +0200

    useEffect-1

--- src/lezioni/2-useEffect/inizio/1-useEffect-basi.js
+++ src/lezioni/2-useEffect/inizio/1-useEffect-basi.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const useEffectBasi = () => {
+  const [value, setValue] = React.useState(0);
+  useEffect(() =>{
+    console.log("Eccomi");
+    if (value == 0){
+      document.title = "UseEffect"
+    } else {
+      document.title = `Nuovo messaggio: ${value}`
+    }
+  })
+  const aumenta = () => {
+    setValue((oldValue) => oldValue + 1);
+  }
   return (
-    <div>
-      <h4>UseEffect Componente Base</h4>
+    <div className="item">
+      <h4>Value : <span>{value}</span></h4>
+      <button className="btn btn-info" onClick={aumenta}>Aumenta</button>
     </div>
   );
 };
