1f6d6c2f11b7c5ec411452000f1b7ee62dfa2078
maicolkutchoukov <maicol.j.95@gmail.com>
2024-07-06 16:27:08 +0200

    Completato useState-3

--- src/lezioni/1-useState/inizio/3-useState-array.js
+++ src/lezioni/1-useState/inizio/3-useState-array.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import {data} from '../../../data'
 
 const ArrayState = () => {
-  return (
-    <>
-      <h3> UseState with Array Components</h3>
-    </>
-  );
+    console.log(data)
+    const [people, setPeople] = useState(data);
+    const removeItem = (id) => {
+        let newPeople = people.filter(person => person.id !== id);
+        setPeople(newPeople);
+    }
+    return (
+        <>
+        {
+            people.map(person => {
+            const {id, name} = person;
+            return (
+                <div key={id} className="item shadow">
+                    <h3>{name}</h3>
+                    <button 
+                        className="btn delete-button"
+                        onClick={() => removeItem(id)}
+                    >
+                        x
+                    </button>
+                </div>
+            );
+            })
+        }
+        </>
+    );
 };
 
 export default ArrayState;
