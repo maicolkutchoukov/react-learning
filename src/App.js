acef083a200ff3f204ca3cbc24f39cef59da5c23
maicolkutchoukov <maicol.j.95@gmail.com>
2024-07-07 01:00:49 +0200

    Completato progetto-1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Inizio from "./lezioni/1-useState1/inizio/1-errore-base";
+import Inizio from "./progetti/progetto-1/progetto1";
+
 
 function App() {
   return (
     <div className="App">
-      <section className="container text-center my-3">
+      <h1 className="text-center my-4">Prossimi incontri</h1>
+      <section className="container text-center my-3 m-auto d-flex flex-column align-items-center w-75">
         <Inizio />
       </section>
     </div>
---
+++ src/progetti/progetto-1/progetto1.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import { dati } from '../../data'
+
+const Progetto1 = () => {
+    console.log(dati);
+    const [people, setPeople] = useState(dati);
+    console.log(people);
+    const removeItem = (id) => {
+        let newPeople = people.filter(person => person.id !== id);
+        setPeople(newPeople);
+    }
+    const removeAll = () => {
+        setPeople([]);
+    }
+    const reloadPeople = () => {
+        setPeople(dati)
+    }
+  return (
+    <>
+        {
+            people.map(person => {
+                const {id, nome, stato, img} = person;
+                return (
+                    <div key = {id} className='item m-0 w-75 bg-dark text-white text-start'>
+                        <div className='img-container'>
+                            <img src={img} alt="immagine" className='rounded-circle'/>
+                        </div>
+                        <div className='description px-4 flex-grow-1'>
+                            <h4>{nome}</h4>
+                            <p>{stato}</p>
+                        </div>
+                        <div className='btn-container'>
+                            <button className='btn delete-button' onClick={()=>removeItem(id)}>X</button>
+                        </div>
+                    </div>
+                )
+            })
+        }
+        <div className='d-flex justify-content-around w-75 mt-3'>
+            <button className='btn btn-info border-dark' onClick={reloadPeople}>Reload</button>
+            <button className='btn delete-button border-dark' onClick={removeAll}>Delete All</button>
+        </div>
+    </>
+  )
+}
+
+export default Progetto1;
