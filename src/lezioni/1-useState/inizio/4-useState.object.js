947e63d827a51a436af465ae59211d16b64aa3c6
maicolkutchoukov <maicol.j.95@gmail.com>
2024-07-06 16:42:14 +0200

    Completato useState-4

--- src/lezioni/1-useState/inizio/4-useState.object.js
+++ src/lezioni/1-useState/inizio/4-useState.object.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const StateObject = () => {
-  return <div>Object Componente UseState</div>;
+    /* const [name, setName] = useState("Maicol");
+    const [age, setAge] = useState(28);
+    const [saluto, setSaluto] = useState("Ciao"); */
+    const [person, setPerson] = useState({
+        name: "Maicol",
+        age: 28,
+        saluto: "Ciao"
+    })
+
+    const cambiaSaluto = () => {
+        /* setSaluto("Tanti auguri, ho fatto oggi gli anni!");
+        setAge(29); */
+        setPerson({
+            ...person,
+            age: 29,
+            saluto: "Ho fatto gli anni!"
+        })
+    }
+    return (
+        <div className="item shadow">
+            <div>
+                <h5>{person.name}</h5>
+                <h6>{person.age}</h6>
+                <h6>{person.saluto}</h6>
+            </div>
+            <button className="btn btn-primary" onClick={cambiaSaluto}>Cambia Saluto!</button>
+        </div>
+    );
 };
 
 export default StateObject;
