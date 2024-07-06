82edd7528e99a9a7c8897ee9a58a05ada221f88c
maicolkutchoukov <maicol.j.95@gmail.com>
2024-07-06 17:34:19 +0200

    Completato useState-5

--- src/lezioni/1-useState/inizio/5-useState-contatore.js
+++ src/lezioni/1-useState/inizio/5-useState-contatore.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 const CounterComponent = () => {
+  const [counter, setCounter] = useState(0);
+  /* incrementCounter(counter) = counter++;
+  decrementCounter(counter) = counter--;
+  resetCounter(counter) = 0 */
+  const reset = () => {
+    setCounter(0);
+  };
+  const aumenta = () =>{
+    setCounter(oldValue => {
+      if (oldValue + 1 == 5){
+        return oldValue
+      }
+      return oldValue + 1
+    })
+    
+  };
   return (
-    <div>
-      <h3> Conter Component</h3>
+    <div className="bg-white shadow rounder py-5 w-75 col-6 offset-3">
+      <h2>{counter}</h2>
+      <div className="d-flex justify-content-between mb-3">
+        <button className="btn btn-info ms-4"onClick={() => setCounter(counter - 1)}>Diminuisci</button>
+        <button className="btn btn-info me-4" onClick={aumenta}>Aumenta</button>
+      </div>
+      <div className="d-flex justify-content-center">
+        <button className="btn reset-button" onClick={reset}>Azzera</button>
+      </div>
     </div>
   );
 };
