5d3a111d91970e01d5c02e8878ed98ebd572e0b7
maicolkutchoukov <maicol.j.95@gmail.com>
2024-07-07 03:20:14 +0200

    render & show

--- src/lezioni/3-render-condizionale/inizio/1-render-multiplo.js
+++ src/lezioni/3-render-condizionale/inizio/1-render-multiplo.js
@@ -1,13 +1,63 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 const url = "https://api.github.com/users/QuincyLarson";
 
 const ConditionalCompining = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
+
+  const getData = async () => {
+    setIsError(false)
+    setIsLoading(true);
+    try {
+      const response = await axios.get(url);
+      console.log(response);
+      setUser(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false)
+      setIsError(true);
+    }
+    
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+  if (isLoading){
+    return <Loading/>
+  }
+  if (isError){
+    return <ErrorComponent/>
+  }
   return (
-    <div>
-      <h2>Conditional Rendering</h2>
-    </div>
+    <article className="card d-flex align-items-center p-5 shadow mt-5">
+      <img 
+        src={user.avatar_url}
+        alt={user.name}
+        style={{ width: "100px", height: "100px" }}
+        className="rounded-circle my-4 shadow"
+      />
+      <h2>{user.name}</h2>
+    </article>
   );
 };
 
+const Loading = () => {
+  return (
+    <div>
+      <h2>Loading...</h2>
+    </div>
+  )
+}
+const ErrorComponent = () => {
+  return (
+    <div>
+      <h2>Sorry, there are an error...</h2>
+      
+    </div>
+  )
+}
 export default ConditionalCompining;
